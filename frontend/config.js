a08a44e8d2070d05350a5ba808ecf5b6ae3f4c25
Syam-SriBalaji-T <syamsribalaji@gmail.com>
2025-08-27 11:29:16 +0530

    some changes

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const BACKEND_URL = "https://quick-test-platform.vercel.app/";
-export const FRONTEND_URL = "https://quick-test-platform.netlify.app/";
+export const BACKEND_URL = "https://quick-test-platform.vercel.app";
+export const FRONTEND_URL = "https://quick-test-platform.netlify.app";
